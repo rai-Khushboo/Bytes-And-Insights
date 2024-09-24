afb189b0454532a731f2142da5dc71341e435c2e
rai-Khushboo <khushboo83687@gmail.com>
2024-09-24 20:34:34 +0530

    added required pages for websites

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,10 @@ import {
 } from "react-router-dom";
 //import home page from the pages
 import Home from './pages/Home.jsx'
+import Blogs from './pages/Blogs.jsx';
+import About from './pages/About.jsx';
+import Services from './pages/Services.jsx';
+import Contact from './pages/Contact.jsx';
 
 const router = createBrowserRouter([
   {
@@ -18,7 +22,23 @@ const router = createBrowserRouter([
       {
         path:"/" ,
         element:<Home />
-      }
+      },
+      {
+        path:"/blogs",
+        element:<Blogs />
+      },
+      {
+        path:"/about",
+        element:<About />
+      },
+      {
+        path:"/services",
+        element:<Services />
+      },
+      {
+        path:"/contact",
+        element:<Contact />
+      },
     ]
   },
 ]);
---
+++ src/pages/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+      Serviecs
+    </div>
+  )
+}
+
+export default Services
