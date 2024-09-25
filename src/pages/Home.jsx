1f9aa76045710f90e98802ad10dad5769cfc35c2
rai-Khushboo <khushboo83687@gmail.com>
2024-09-26 00:13:21 +0530

    Debugging for banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Banner from '../components/Banner'
 const Home = () => {
   return (
-    <div className='px-4 py-32'>
+    <div className=''>
       <Banner />
     </div>
   )
